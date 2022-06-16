e27fac0ca53a981392a1ef200ad9f7e378cc53f5
Greg Pendleton <gpendleton@gmail.com>
2022-06-16 17:22:08 +0000

    add public folder

--- myApp.js
+++ myApp.js
@@ -1,5 +1,16 @@
 let express = require('express');
 let app = express();
+console.log("Hello World");
+app.use('/public', express.static(__dirname + '/public'))
+
+app.get('/', (req, res)=>{
+        // res.send("Hello Express")
+        const absolutePath = __dirname +               
+          '/views/index.html'
+        res.sendFile(absolutePath)
+})
+
+
 
 
 

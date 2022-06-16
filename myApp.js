655b9ef24671bcc23218c437c603efee1ac56309
Greg Pendleton <gpendleton@gmail.com>
2022-06-16 23:59:45 +0000

    Stuff

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-console.log("Hello World");
+
 app.use('/public', express.static(__dirname + '/public'))
 
 app.get('/', (req, res)=>{
@@ -9,6 +9,8 @@ app.get('/', (req, res)=>{
           '/views/index.html'
         res.sendFile(absolutePath)
 })
+
+app.get('json', res.json({"message": "Hello json"}))
 
 
 
